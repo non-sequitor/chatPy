125f941667eac2b2c2526450260c724956526f9b
Ayushi <67626681+non-sequitor@users.noreply.github.com>
2025-09-19 04:23:17 +0530

    Update settings.py

--- chatPy/settings.py
+++ chatPy/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b_ifx2z$a5$d_aj@ds+g^g8lgf-(aes5&&f_zq^(@v-@qivv6-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
@@ -116,6 +116,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
